18910772c5b9dcdaf0c1e752cd2552fb61c30c90
Shuyi Qiu <shuyi.qiu@duke.edu>
2024-01-15 17:00:02 +0000

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,80 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# psidread
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of psidread is to provide a user-friendly approach to
+streamline the management, creation, and formatting of panel data from
+the Panel Study of Income Dynamics (PSID).
+
+## Installation
+
+You can install the development version of psidread from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("Qcrates/psidread")
+```
+
+## Usage
+
+### Overview
+
+The [Panel Study of Income Dynamics](https://psidonline.isr.umich.edu/)
+(PSID) is the longest running longitudinal household survey in the
+world, which provides invaluable data covering numerous topics including
+marriage, income, wealth, health and etc. However, the process of
+converting raw PSID data files into datasets ready for analysis is quite
+complex and challenging, especially for new users.
+
+This package is developed with the purpose of addressing these
+challenges within only R environment without additional assistance from
+other statistical programming softwares. By bridging these gaps, the
+package aims to make PSID datasets more usable and manageable for
+researchers and analysts.
+
+### Introduction
+
+What `psidread` package is created to help:
+
+- Create a table of data structure across multiple waves using the text
+  that can be copied and pasted from the website
+
+- Unzip and convert the zipped files without additional help of other
+  software
+
+- Read and merge the data files from multiple waves
+
+- Rename and reshape the dataset to fit the need for advanced analysis
+
+### Example Workflow
+
+``` r
+# Step 1: Create the table of data structure ----
+psid_varlist = c(" hh_age || [13]ER53017 [17]ER66017", " p_age || [13]ER34204")
+str_df <- psid_str(varlist = psid_varlist, type = "separated")
+
+# Step 2: Unzip and convert the ascii data ----
+input_directory <- "your/input/directory"
+output_directory <- "your/export/directory"
+psid_unzip(indir = input_directory,
+           exdir = output_directory,
+           zipped = TRUE,
+           type = "package",
+           filename = NA)
+
+# Step 3: Read and merge data ----
+data_directory <- "your/folder/with/converted/data"
+psid_df <- psid_read(indir = data_directory, str_df = str_df,idvars = c("ER30000"),type = "package",filename = NA)
+
+# Step 4: Rename and reshape the data ----
+df <- psid_reshape(psid_df = psid_df, str_df = str_df, shape = "long", level = "individual")
+df
+```
+
+Please refer to the vignettes for the detailed instructions on how to
+build your own dataset from PSID using this package.
---
+++ inst/doc/my-vignette.R
@@ -0,0 +1,100 @@
+## ----eval = FALSE-------------------------------------------------------------
+#  devtools::install_github(repo = "Qcrates/psidread")
+
+## -----------------------------------------------------------------------------
+library(DiagrammeR)
+library(psidread)
+
+## ----echo=FALSE, fig.width=10, fig.height=10----------------------------------
+DiagrammeR("
+graph TB;
+    A[Dataset Downloaded] --> B[psid_str, 3];
+    B --> C{Type};
+    C --> |Packaged| D{.rda files prepared?};
+    C --> |Single| E{.rda files prepared?};
+    D --> |No| F[psid_unzip, 4.1];
+    D --> |Yes| G[psid_read, 5.1];
+    F --> G;
+    E --> |No| H[psid_unzip, 4.2];
+    E --> |Yes| I[psid_read, 5.2];
+    H --> I;
+    G --> J[psid_reshape, 6];
+    I --> J;
+    J --> L((Output));
+    
+    style A fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style B fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style C fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style D fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style E fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style F fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style G fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style H fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style I fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style J fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+    style L fill:#ffffff,stroke:#4d4d4d,stroke-width:1px;
+")
+
+## -----------------------------------------------------------------------------
+psid_str(
+  varlist = c("hh_educ || [13]ER58223 [15]ER65459 [17]ER71538 [19]ER77599",
+              "num_child || [13]ER53020 [15]ER60021 [17]ER66021 [19]ER72021"),
+  type = "separated"
+)
+
+## -----------------------------------------------------------------------------
+psid_varlist <- "|| religion_hh /// Household head's religious preference
+	[97]ER11895 [99]ER15977 [01]ER20038 [03]ER23474 [05]ER27442 [07]ER40614 ///
+	|| denom_hh /// Household head's religious denominations
+	[97]ER11896 [99]ER15978 [03]ER23475 [05]ER27443 [07]ER40615 ///"
+psid_str(
+  varlist = psid_varlist,
+  type = "integrated"
+)
+
+## -----------------------------------------------------------------------------
+input_directory <- system.file(package = "psidread","extdata")
+output_directory <- system.file(package = "psidread","extdata")
+psid_unzip(indir = input_directory,
+           exdir = output_directory,
+           zipped = TRUE,
+           type = "package",
+           filename = NA)
+
+## -----------------------------------------------------------------------------
+psid_unzip(indir = input_directory,
+           exdir = output_directory,
+           zipped = FALSE,
+           type = "package",
+           filename = NA)
+
+## -----------------------------------------------------------------------------
+psid_unzip(indir = input_directory,
+           exdir = output_directory,
+           zipped = TRUE,
+           type = "single",
+           filename = "J327825.zip")
+
+## -----------------------------------------------------------------------------
+psid_varlist = c(" hh_age || [13]ER53017 [17]ER66017", " p_age || [13]ER34204")
+str_df <- psid_str(varlist = psid_varlist, type = "separated")
+input_directory <- system.file(package = "psidread","extdata")
+psid_df <- psid_read(indir = input_directory, str_df = str_df,idvars = c("ER30000"),type = "package",filename = NA)
+str(psid_df)
+
+## -----------------------------------------------------------------------------
+psid_df <- psid_read(indir = input_directory, str_df = str_df,idvars = c("ER30000"),type = "single",filename = "J327825")
+str(psid_df)
+
+## -----------------------------------------------------------------------------
+df <- psid_reshape(psid_df = psid_df, str_df = str_df, shape = "long", level = "individual")
+df
+
+## -----------------------------------------------------------------------------
+df <- psid_reshape(psid_df = psid_df, str_df = str_df, shape = "wide", level = "individual")
+df
+
+## -----------------------------------------------------------------------------
+df <- psid_reshape(psid_df = psid_df, str_df = str_df, shape = "long", level = "household")
+df
+
